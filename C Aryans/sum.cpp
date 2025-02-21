61eeb4c00934ac483e08da708e3cc7d937aadd08
duhrsh <126384815+duhrsh@users.noreply.github.com>
2025-02-21 20:20:46 +0530

    Update sum.cpp

--- C Aryans/sum.cpp
+++ C Aryans/sum.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 
 int main() {
-    // Declare variables to store the numbers
     int num1, num2;
-
-    // Prompt the user to enter the first number
+    
     std::cout << "Enter the first number: ";
     std::cin >> num1;
 
-    // Prompt the user to enter the second number
     std::cout << "Enter the second number: ";
     std::cin >> num2;
 
-    // Calculate the sum of the two numbers
     int sum = num1 + num2;
-
-    // Output the sum to the user
     std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << std::endl;
-
-    // Indicate that the program ended successfully
+    
     return 0;
 }
